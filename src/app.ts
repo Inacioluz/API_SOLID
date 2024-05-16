8c1f4e6252ea13fbdf1f8f22b86e492b612501da
Inacio Silva <joseinaciolds@gmail.com>
2024-05-16 08:24:21 -0300

    Controller de criação de academia OK

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
-import fastify from 'fastify';
-import { appRoutes } from './http/routes';
-import { ZodError } from 'zod';
-import { env } from './env';
-import fastifyJwt from '@fastify/jwt';
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from '@/env'
+import { usersRoutes } from '@/http/controllers/users/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 
-export const app =  fastify()
 
-app.register(fastifyJwt,{
-    secret: env.JWT_SECRET,
-  })
+export const app = fastify()
 
-app.register(appRoutes)
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
-    if( error instanceof ZodError) {
-        return reply
-        .status(400)
-        .send({ menssage: 'Validation Error. ', issues: error.format()})
-    }
-    if(env.NODE_ENV !== 'production'){
-        console.error(error)
-    } else {
-        // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
-    }
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
 
-    return reply.status(500).send({ message: 'Internal server error.'})
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
